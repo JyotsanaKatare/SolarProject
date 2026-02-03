f6bb9eeb3b416e061241dbdb4db399f860ad556f
JyotsanaKatare <122454192+JyotsanaKatare@users.noreply.github.com>
2026-02-03 15:22:05 +0530

    Update formRoutes.js

--- API/routes/formRoutes.js
+++ API/routes/formRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import form from '../controllers/formController.js';
 
 console.log("Form routes loaded");
-router.post("/free_quote", form.freeQuote);
+router.get("/free_quote", form.freeQuote);
 router.post("/contact_us", form.contactUs);
 
 export default router;
