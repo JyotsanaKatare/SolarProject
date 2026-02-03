537e6962fc6d17505d5df5833bec5aff6105263d
JyotsanaKatare <122454192+JyotsanaKatare@users.noreply.github.com>
2026-02-03 15:30:07 +0530

    Update index.js

--- API/index.js
+++ API/index.js
@@ -8,13 +8,7 @@ import routes from './routes/formRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({
-    origin: [
-        "https://appowers.in",
-        "http://localhost:5173",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.json());
