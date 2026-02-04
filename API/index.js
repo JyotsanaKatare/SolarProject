fadbf044327213e60165c0f4452d86d39c6d18b9
Jyotsana Katare <jyotsanakatare63@gmail.com>
2026-02-04 11:44:23 +0530

    cores issue solve

--- API/index.js
+++ API/index.js
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 
 // Pre-flight request handle karne ke liye (Sabse zaroori)
-app.options('*', cors());
+app.options('(.*)', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
